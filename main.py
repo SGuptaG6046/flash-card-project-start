218a342b01bae5e77a8faf3a0932e42230f3a450
SGuptaG6046 <trilokigupta121@gmail.com>
2023-09-08 22:23:19 +0530

    commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+gui = Tk()
+gui.title("Flashy")
+gui.config(bg=BACKGROUND_COLOR,padx=50,pady=50)
+
+
+# Create Canvas
+canvas = Canvas(width=800, height=526)
+
+
+
+gui.mainloop()
